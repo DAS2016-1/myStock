90673dce390859a481742514ae543a3b4fd7d25b
Wilton Rodrigues <braynwilton@gmail.com>
2016-06-15 14:13:21 -0300

    Adding sales app

--- sales/models.py
+++ sales/models.py
@@ -1,3 +1,5 @@
 from django.db import models
+from products.models import *
 
-# Create your models here.
+class Sale(models.Model):
+    sale_date = models.DateField()
--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse
+from products.models import *
+from sales.models import *
 
-# Create your views here.
+def index(request):
+    products = Product.objects.all
+    return render(request, 'sales/sales.html', {'products': products})
